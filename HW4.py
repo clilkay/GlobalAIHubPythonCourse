d4a7a52fdc5e73d3f665c957cda4ff2e3f07663f
clilkay <77379528+clilkay@users.noreply.github.com>
2021-02-06 15:27:05 +0300

    Add files via upload

---
+++ HW4.py
@@ -0,0 +1,29 @@
+class Animal:
+    def __init__(self,name,colour,leg):
+        self.name=name
+        self.colour=colour
+        self.leg=leg
+    def Show(self):
+        print("Name :"+self.name+" "+"Colour :"+self.colour+" "+"Leg :"+str(self.leg))
+class Dog(Animal):
+    def __init__(self, name, colour, leg, sound):
+        super().__init__(name, colour, leg)
+       
+        self.sound=sound
+
+    def Sound1(self):
+        print(  "It's sound is :"+self.sound)
+
+class Cat(Animal):
+    def __init__(self, name, colour, leg, sound2):
+      super().__init__(name, colour, leg)
+      self.sound2=sound2
+    
+      
+    def Sound2(self):
+        print("It's sound is :"+self.sound2)
+
+cat1=Cat("b","b",4,"miyav")
+
+cat1.Show()
+cat1.Sound2()
